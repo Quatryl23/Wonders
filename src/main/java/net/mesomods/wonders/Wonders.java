48bc87f9433e2b89e89cf820f3a2601db938633e
Mesophil <170469614+Quatryl23@users.noreply.github.com>
2026-08-17 20:51:22 +0200

    Initial commit

---
+++ src/main/java/net/mesomods/wonders/Wonders.java
@@ -0,0 +1,10 @@
+package net.mesomods.wonders;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Wonders implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/mesomods/wonders/client/WondersClient.java
@@ -0,0 +1,10 @@
+package net.mesomods.wonders.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WondersClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
